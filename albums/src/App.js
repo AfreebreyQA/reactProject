b0039ab760ddb492f6f2f5a3a43e31402946ef64
AfreebreyQA <andrew.freebrey@qa.com>
2017-11-09 14:58:43 +0000

    Rendered hello to screen as part of a table component

--- albums/src/App.js
+++ albums/src/App.js
@@ -8,14 +8,32 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Album Search</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <TableComponent />
       </div>
     );
   }
 }
+
+class TableComponent extends Component {
+  render() {
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              hello
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    )
+  }
+}
+
 
 export default App;
